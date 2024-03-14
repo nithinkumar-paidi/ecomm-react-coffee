aa7e5152375766525d99d73bc9ff33b4dd859626
npx <nithinpaidi@lyrostech.com>
2024-03-14 16:09:38 +0530

    just now updated

--- src/App.js
+++ src/App.js
@@ -20,23 +20,22 @@ function App() {
   return (
     <div className="App">
       <UserContext.Provider value={val}>
-        {flag === 0 || flag === 1 ? (
+        {/* {flag === 0 || flag === 1 ? (
           <Login />
-        ) : (
+        ) : ( */}
           <AppContextProvider>
             <Router>
               <Navbar />
               <hr></hr>
               <Routes>
                 <Route path={`${PATH}/`} index element={<Products />} />
-                <Route path={`${PATH}/Cart`}  element={<Cart />} />
                 <Route path={`${PATH}/order`} element={<Orders />} />
-               
+                <Route path={`${PATH}/cart`} element={<Cart />} />
               </Routes>
               <Footer />
             </Router>
           </AppContextProvider>
-        )}
+        {/* )} */}
       </UserContext.Provider>
     </div>
   );
